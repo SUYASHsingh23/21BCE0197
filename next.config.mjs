70533bac547c9a6d95d9135c37e11aaa9f703f1f
SUYASHsingh23 <suyashsingh138@gmail.com>
2024-08-25 11:38:56 +0530

    Initial commit for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    assetPrefix: '.',
+  }
+  
+  module.exports = nextConfig
 
 export default nextConfig;
